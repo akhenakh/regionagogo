58917156b8afdd17c7a973321c055ed605d077c7
Fabrice Aneche <akh@nobugware.com>
2016-06-08 18:46:07 -0400

    field flag parsing

--- cmd/gendata/main.go
+++ cmd/gendata/main.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/akhenakh/regionagogo"
 )
 
+// FieldFlag reusable parse Value to create import command
+type FieldFlag struct {
+	Fields []string
+}
+
+func (ff *FieldFlag) String() string {
+	return fmt.Sprint(ff.Fields)
+}
+
+func (ff *FieldFlag) Set(value string) error {
+	if len(ff.Fields) > 0 {
+		return fmt.Errorf("The field flag is already set")
+	}
+
+	ff.Fields = strings.Split(value, ",")
+	return nil
+}
+
 func main() {
-	var ff regionagogo.FieldFlag
+	var ff FieldFlag
 
 	filename := flag.String("filename", "", "A geojson file")
 	debug := flag.Bool("debug", false, "Enable debug")
